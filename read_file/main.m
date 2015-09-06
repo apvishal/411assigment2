3db2c78db34243aedc8bc0d7e7093755e99c5782
Vishal Patel <vpatel589@csu.fullerton.edu>
2015-09-05 20:53:57 -0700

    Initial Commit

---
+++ read_file/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  read_file
+//
+//  Created by Vishal Patel on 9/5/15.
+//  Copyright (c) 2015 Vishal Patel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
